d546bd55d50972dec0acf7c46b3a4a8009a3c35f
Parth Parikh <168380101+ParthP29@users.noreply.github.com>
2026-04-01 09:05:25 +1300

    adding the main file

---
+++ CoinFlipper.py
@@ -0,0 +1,19 @@
+from random import choice
+
+def flip_a_coin():
+    coin_flip = choice(["Heads", "Tails"])
+    print(f"You flipped {coin_flip}.")
+    if coin_flip == "Heads":
+        print("You won!")
+    else:
+        print("You lost!")
+    
+def print_how_to():
+    print("Heads you win\nTails you lose\n")
+
+def main():
+    print_how_to()
+    for _ in range(10):
+        flip_a_coin()
+
+main()
